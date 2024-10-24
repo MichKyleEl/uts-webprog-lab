a00bd05d85ba74799e6f6be9aa69a90b1d0e60de
BenArief <benarief29@gmail.com>
2024-10-25 00:36:48 +0700

    Menambahkan login_process.php untuk mengecek apakah user sudah memiliki akun atau belum

---
+++ login_process.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+require_once('db.php');
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM user_info WHERE username = ?";
+$stmt = $db->prepare($sql);
+$stmt->execute([$username]);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if(!$row){
+    header('location: login.php?error=User not found');
+    exit;
+}else{
+    if(!password_verify($password, $row['password'])){
+        header('location: login.php?error=Wrong password');
+        exit;
+    }else{
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
+        header('location: index.php');
+    }
+}
+?>
